3967e9774e0494c9cb1e7cbbae84534c725823cd
Sakshi Ganesh Danej <sakshi1314.d@gmail.com>
2023-11-22 23:49:37 +0530

    first commit - frontend

---
+++ src/components/HeroSection.js
@@ -0,0 +1,109 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { Button } from "../styles/Button";
+
+const HeroSection = ({ myData }) => {
+  const { name } = myData;
+
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="grid grid-two-column">
+          <div className="hero-section-data">
+            <h3 className="intro-data">Welcome to </h3>
+            <h1> {name} </h1>
+            <p>
+            Palette Pros is your exclusive destination to explore and acquire captivating artworks
+             by Sakshi Danej. Immerse yourself in a world of digital art, watercolors, and acrylic 
+             creations that reflect my unique artistic vision. Start your art journey with us today
+              and bring home a piece of creativity.
+            </p>
+            <NavLink>
+              <Button>shop now</Button>
+            </NavLink>
+          </div>
+          {/* our homepage image  */}
+          <div className="hero-section-image">
+            <figure>
+              <img
+                src="images/hero.jpg"
+                alt="hero-section-tasveer"
+                className="img-style"
+                style={{height:"450px"}}
+              />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  padding: 12rem 0;
+
+  img {
+    min-width: 10rem;
+    height: 10rem;
+  }
+
+  .hero-section-data {
+    p {
+      margin: 2rem 0;
+    }
+
+    h1 {
+      text-transform: capitalize;
+      font-weight: bold;
+    }
+
+    .intro-data {
+      margin-bottom: 0;
+      color : #F78CA2;
+    }
+  }
+
+  .hero-section-image {
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  figure {
+    position: relative;
+
+    &::after {
+      content: "";
+      width: 60%;
+      height: 60%;
+      background-color: #F78CA2;
+      position: absolute;
+      left: 50%;
+      top: -5rem;
+      z-index: -1;
+    }
+  }
+  .img-style {
+    width: 100%;
+    height: auto;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid {
+      gap: 10rem;
+    }
+
+    figure::after {
+      content: "";
+      width: 50%;
+      height: 100%;
+      left: 0;
+      top: 10%;
+      /* bottom: 10%; */
+      background-color: rgba(81, 56, 238, 0.4);
+    }
+  }
+`;
+
+export default HeroSection;
